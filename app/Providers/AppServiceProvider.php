f5408b398f6c39f076e838e06e57d51fd1f72ce4
Phil <xuyuwei19940909@gmail.com>
2025-01-14 20:55:37 -0800

    build heroku files and settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($googleCredentials = env('GOOGLE_APPLICATION_CREDENTIALS_JSON')) {
+            $tempPath = storage_path('app/google-service-key.json');
+            File::put($tempPath, base64_decode($googleCredentials));
+
+            // 设置 Google Cloud SDK 环境变量
+            putenv("GOOGLE_APPLICATION_CREDENTIALS=$tempPath");
+        }
     }
 }
